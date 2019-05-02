369e989af618fd8a7ce41e929c4ee3457054913f
Priyank Patel <priyankpatel4903@gmail.com>
2019-05-02 10:15:49 -0400

    Add files via upload
    test

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i= 0; i < 10; i++){
+			System.out.println("count -- > " + i);
+		}
+	}
+}

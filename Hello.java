7c1c0a2d4a346984811616b2ce5975165940e717
priyankpatel22 <priyankpatel4903@gmail.com>
2019-05-24 10:49:55 -0400

    Add chnages..

--- Hello.java
+++ Hello.java
@@ -4,10 +4,10 @@ import java.io.*;
 
 class Hello
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		for(int i= 0; i < 10; i++){
-			System.out.println("count -- > " + i);
-		}
-	}
+	// public static void main (String[] args) throws java.lang.Exception
+	// {
+	// 	for(int i= 0; i < 10; i++){
+	// 		System.out.println("count -- > " + i);
+	// 	}
+	// }
 }

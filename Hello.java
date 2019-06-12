6b781926377f1b4df3082399be29b36929fddd30
priyankpatel22 <priyankpatel4903@gmail.com>
2019-06-12 14:17:19 -0400

    new

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class Hello
 {
-	//public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) throws java.lang.Exception
 	{
 		for(int i= 0; i < 10; i++){
 			System.out.println("count -- > " + i);
